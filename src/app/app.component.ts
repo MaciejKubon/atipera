4f36971a2c748470e5e6b6f74c028c2790d09413
Maciej Kubon <maciejk2233@gmail.com>
2024-09-18 22:13:06 +0200

    add data loading and implementation in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableComponent } from './component/table/table.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
